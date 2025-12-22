618b028176f02ef428f50952a02f0d101c0dae38
OTS <ots@OTSs-Mac-mini.lan>
2025-12-22 15:57:55 +0400

    Fix: Add comment clarifying safe bootstrap operations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Safe operations that don't require database
         Blade::withoutDoubleEncoding();
         Paginator::useBootstrap();
 
